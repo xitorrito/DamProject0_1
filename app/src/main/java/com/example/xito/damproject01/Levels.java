1644f61dffe4c66246a0b7979dbd63c47b94b4e7
Xito Gonzalez <xitomasterpiece@gmail.com>
2016-04-20 16:59:12 +0200

    Update Levels.java

--- app/src/main/java/com/example/xito/damproject01/Levels.java
+++ app/src/main/java/com/example/xito/damproject01/Levels.java
@@ -1,1 +1,33 @@
 
+public class Levels{
+  
+  
+  public Levels(){
+    
+  }
+  
+  // maximum experience points
+  public int max_exp;
+
+  // maximum level
+  public int max_level;
+
+  // get a level minimum exp
+  public int getExperience (int level)
+  {
+      return Math.ceil((double)max_exp / Math.pow(2, max_level-level));
+  }
+  
+  public int getExpForNextLevel(int level){
+      return 
+  }
+
+  // get current level
+  public int level getLevel (int exp)
+  {
+      return Math.floor((double)max_level - (Math.log2(max_exp) - Math.log2(exp)));
+  }
+  
+  
+  
+}
