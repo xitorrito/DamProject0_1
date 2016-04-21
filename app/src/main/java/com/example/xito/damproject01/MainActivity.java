a9d1afbbf64edcafe439cad1cf9057e84807478e
Xito <xitomasterpiece@gmail.com>
2016-04-21 23:27:40 +0200

    Iconos tabs

--- app/src/main/java/com/example/xito/damproject01/MainActivity.java
+++ app/src/main/java/com/example/xito/damproject01/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.xito.damproject01;
 
+import android.database.Cursor;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -30,36 +31,48 @@ public class MainActivity extends AppCompatActivity {
         setupViewPager(viewPager);
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
+
         tabLayout.setupWithViewPager(viewPager);
-        
-        
-        
-        
-        
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false); //Remove the HomeAsUpButton arrow
+
+        setTabIcons(tabLayout);
+
+
+
+
+
     }
-    
+    /*
     public void setPlayerStates() {
-        Cursor cursor = bd.query("player",nivel, null,null,null,null,null);
+        Cursor cursor = db.query("player",nivel, null,null,null,null,null);
         //Vamos a la primera fila.
         cursor.moveToFirst();
         //Obtenemos el dato de la primera (y única) columna.
         int coins = cursor.getInt(0);
-        
+
         Cursor cursor = bd.query("player",exp, null,null,null,null,null);
         //Vamos a la primera fila.
         cursor.moveToFirst();
         //Obtenemos el dato de la primera (y única) columna.
         int level = cursor.getInt(0);
-        
-        
-    }
+
+}
+        */
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new Fragment1(), "ONE");
-        adapter.addFragment(new Fragment2(), "TWO");
-        adapter.addFragment(new Fragment3(), "THREE");
+        adapter.addFragment(new Fragment1(), "");
+        adapter.addFragment(new Fragment2(), "");
+        adapter.addFragment(new Fragment3(), "");
         viewPager.setAdapter(adapter);
+
+
+    }
+
+    public void setTabIcons(TabLayout tabIcons) {
+        tabLayout.getTabAt(0).setIcon(R.drawable.pc_icon );
+        tabLayout.getTabAt(1).setIcon(R.drawable.coin_icon);
+        tabLayout.getTabAt(2).setIcon(R.drawable.tasks_icon);
     }
 
     class ViewPagerAdapter extends FragmentPagerAdapter {
--- app/src/main/java/com/example/xito/damproject01/Player.java
+++ app/src/main/java/com/example/xito/damproject01/Player.java
@@ -1,28 +1,30 @@
-public class Player{
-    public Player{
-        
-    }
-    
-    public void getPlayerStates() {
-        Cursor cursor = bd.query("player",nivel, null,null,null,null,null);
-        //Vamos a la primera fila.
-        cursor.moveToFirst();
-        //Obtenemos el dato de la primera (y única) columna.
-        int coins = cursor.getInt(0);
-        
-        Cursor cursor = bd.query("player",exp, null,null,null,null,null);
-        //Vamos a la primera fila.
-        cursor.moveToFirst();
-        //Obtenemos el dato de la primera (y única) columna.
-        int level = cursor.getInt(0);
-        
-        
-    }
-    
-    public int getExpForNextLevel(int level){
-        int expRequired = 40*1.5^(level-1);
-    }
-  
-  
-  
-}
+import android.database.Cursor;
+//
+//public class Player{
+//    public Player{
+//
+//    }
+//
+//    public void getPlayerStates() {
+//        Cursor cursor = bd.query("player",nivel, null,null,null,null,null);
+//        //Vamos a la primera fila.
+//        cursor.moveToFirst();
+//        //Obtenemos el dato de la primera (y única) columna.
+//        int coins = cursor.getInt(0);
+//
+//        Cursor cursor = bd.query("player",exp, null,null,null,null,null);
+//        //Vamos a la primera fila.
+//        cursor.moveToFirst();
+//        //Obtenemos el dato de la primera (y única) columna.
+//        int level = cursor.getInt(0);
+//
+//
+//    }
+//
+//    public int getExpForNextLevel(int level){
+//        int expRequired = 40*1.5^(level-1);
+//    }
+//
+//
+//
+//}
